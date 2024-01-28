6913fb4ddf4366d35336b244fe58f9631fe40390
Karan <46293088+i-k6@users.noreply.github.com>
2024-01-28 14:11:39 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Layout/Header'
+import Footer from './Layout/Footer'
+function App() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="font-abc">Header</div>
+  )
+}
+
+export default Header
